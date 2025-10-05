04dff2a8160b3de0cc2095e1dbc8391cb14b445c
Info Team <info@growsharecapital.com>
2025-10-05 23:17:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "family-financials",
+  "appId": "1:190841971254:web:d213edeb5a466ad8f1ee97",
+  "apiKey": "AIzaSyBLfPD9rwMZF_I9JxyfboA6UAiPTrhjPTs",
+  "authDomain": "family-financials.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "190841971254"
+};
