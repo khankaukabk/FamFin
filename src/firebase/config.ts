a947c114def0c63943db032e5393c91819cf3677
Growshare Capital <info@growsharecapital.com>
2026-01-28 18:42:38 +0000

    This is the specific fix for your local environment. You need to put tha

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "family-financials",
-  "appId": "1:190841971254:web:d213edeb5a466ad8f1ee97",
-  "authDomain": "family-financials.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "190841971254"
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
